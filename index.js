75e70b698403a8df44b7a90d79b544e79f513ff8
aakrishtaa <116445696+aakrishtaa@users.noreply.github.com>
2024-06-06 15:17:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Configuration, OpenAIApi } = require('openai');
-const dotenv = require('dotenv');
-dotenv.config();
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -11,10 +11,10 @@ const openai = new OpenAIApi(configuration);
 async function main() {
     const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: 'what is the capital of India?' }],
+        messages:[{role: "user", content: 'What is the capital of Denmark'}],
     });
-    console.log(chatCompletion.data);
+    console.log(chatCompletion);
+
 }
 
 main();
-
